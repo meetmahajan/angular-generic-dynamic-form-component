e5b6f5b17d9d61b81928bafa3ae1cfc2201cb963
meetmahajan <monoarchos@gmail.com>
2019-09-18 12:51:31 +0530

    Element-DropDown-Creation

--- src/app/element-dropdown.ts
+++ src/app/element-dropdown.ts
@@ -1,2 +1,11 @@
-export class ElementDropdown {
+import { ElementAttribute } from './element-attribute';
+
+export class ElementDropdown extends ElementAttribute<string> {
+  controlTyepe = 'dropdown';
+  options: { key: string, value: string }[] = [];
+
+  constructor(options: {} = {}) {
+    super(options);
+    this.options = options['options'] || [];
+  }
 }
