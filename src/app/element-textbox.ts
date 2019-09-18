52adf76213b5f6d2d60fa108a20eb03a76ca7645
meetmahajan <monoarchos@gmail.com>
2019-09-18 12:46:13 +0530

    Element-Generic-TextBox created

--- src/app/element-textbox.ts
+++ src/app/element-textbox.ts
@@ -1,2 +1,11 @@
-export class ElementTextbox {
+import { ElementAttribute } from './element-attribute';
+
+export class ElementTextbox extends ElementAttribute<string>{
+  controlType = 'textbox';
+  type: string;
+
+  constructor(options: {} = {}) {
+    super(options);
+    this.type = options['type'] || '';
+  }
 }
