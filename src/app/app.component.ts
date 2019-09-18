516cadde4ab8cf65ead394041ad7e435786114c2
Meet Mahajan <rushabhsanghan12@gmail.com>
2019-09-18 15:24:38 +0530

    Clean Code : Phase 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
+import { ElementAttributeService } from './element-attribute.service';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
+  providers: [ElementAttributeService]
 })
-export class AppComponent  {
-  
+export class AppComponent {
+  elementAttribute: any[];
+  constructor(service: ElementAttributeService) {
+    this.elementAttribute = service.getElementAttributes();
+  }
 }
--- src/app/element-attribute.service.ts
+++ src/app/element-attribute.service.ts
@@ -1,8 +1,45 @@
 import { Injectable } from '@angular/core';
 
+import { ElementDropdown } from './element-dropdown';
+import { ElementAttribute } from './element-attribute';
+import { ElementTextbox } from './element-textbox';
+
 @Injectable()
 export class ElementAttributeService {
 
   constructor() { }
+  getElementAttributes() {
 
+    const elementAttributes: ElementAttribute<any>[] = [
+
+      new ElementDropdown({
+        key: 'brave',
+        label: 'Bravery Rating',
+        options: [
+          { key: 'solid', value: 'Solid' },
+          { key: 'great', value: 'Great' },
+          { key: 'good', value: 'Good' },
+          { key: 'unproven', value: 'Unproven' }
+        ],
+        order: 3
+      }),
+
+      new ElementTextbox({
+        key: 'firstName',
+        label: 'First name',
+        value: 'Bombasto',
+        required: true,
+        order: 1
+      }),
+
+      new ElementTextbox({
+        key: 'emailAddress',
+        label: 'Email',
+        type: 'email',
+        order: 2
+      })
+    ];
+
+    return elementAttributes.sort((a, b) => a.order - b.order);
+  }
 }
