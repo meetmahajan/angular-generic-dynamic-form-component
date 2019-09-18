5326c76de68c40c8b5995adb89fc7c100ba2c2e7
meetmahajan <monoarchos@gmail.com>
2019-09-18 12:35:29 +0530

    Element-Attribute-Model Created

--- src/app/element-attribute.ts
+++ src/app/element-attribute.ts
@@ -1,2 +1,35 @@
-export class ElementAttribute {
+export class ElementAttribute<T> {
+  /*
+  Value : set default value of an element
+  label : set label of an element
+  required : field is required : true | false
+  order : arrange elements in sequence
+  controlType : declare wether it is 'TextBox', 'Dropdown', 'TextArea'
+  */
+  value: T;
+  key: string;
+  label: string;
+  required: boolean;
+  order: number;
+  controlType: string;
+
+  /*
+  Initialization of attributes of element and defining default values for above attributes
+  in options
+  */
+  constructor(options: {
+    value?: T,
+    key?: string,
+    label?: string,
+    required?: boolean,
+    order?: number,
+    controlType?: string
+  } = {}) {
+    this.value = options.value;
+    this.key = options.key || '';
+    this.label = options.label || '';
+    this.required = !!options.required;
+    this.order = options.order === undefined ? 1 : options.order;
+    this.controlType = options.controlType || '';
+  }
 }
