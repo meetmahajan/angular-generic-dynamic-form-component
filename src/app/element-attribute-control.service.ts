8d12ab67b5aaa5c53d9d6ecb7bb0a68c45b7c3dc
meetmahajan <monoarchos@gmail.com>
2019-09-18 14:02:20 +0530

    Element Attribute Service created that handles required and returns a FormGroup

--- src/app/element-attribute-control.service.ts
+++ src/app/element-attribute-control.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { ElementAttribute } from './element-attribute';
 
 @Injectable()
 export class ElementAttributeControlService {
 
   constructor() { }
 
+  toFormElementGroup(elementAttributes: ElementAttribute<any>[]) {
+    let group: any = {};
+    elementAttributes.forEach(elementAttribute => {
+      group[elementAttribute.key] = elementAttribute.required ? new FormControl(elementAttribute.value || '', Validators.required) : new FormControl(elementAttribute.value || '');
+      return new FormGroup(group);
+    })
+  }
 }
