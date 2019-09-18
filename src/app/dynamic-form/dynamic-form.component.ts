a673ec1c8728a5707fc342195d7dc64fb2ffda46
meetmahajan <monoarchos@gmail.com>
2019-09-18 14:17:19 +0530

    Dynamic Container Created

--- src/app/dynamic-form/dynamic-form.component.ts
+++ src/app/dynamic-form/dynamic-form.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { ElementAttribute } from '../element-attribute';
+import { ElementAttributeService } from '../element-attribute.service';
 
 @Component({
   selector: 'app-dynamic-form',
   templateUrl: './dynamic-form.component.html',
-  styleUrls: ['./dynamic-form.component.css']
+  styleUrls: ['./dynamic-form.component.css'],
+  providers:[ElementAttributeService]
 })
 export class DynamicFormComponent implements OnInit {
 
-  constructor() { }
+  @Input() elementAttributes: ElementAttribute<any>[] = [];
+  form: FormGroup;
+  payLoad = '';
+
+  constructor(private eas: ElementAttributeService) { }
 
   ngOnInit() {
+    this.form = this.eas.toFormElementGroup(this.elementAttributes);
   }
 
 }
